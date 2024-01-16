ea3269add24e7eb4e923b959389b9f1742adbd3b
Rodrigoaaalves <136371023+Rodrigoaaalves@users.noreply.github.com>
2024-01-16 12:06:59 -0300

    Produto.java

---
+++ Produto.java
@@ -0,0 +1,36 @@
+public class Produto {
+    private String nome;
+    private double preco;
+    private int quantidade;
+
+    public Produto(String nome, double preco, int quantidade) {
+        this.nome = nome;
+        this.preco = preco;
+        this.quantidade = quantidade;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    public void exibirInformacoes() {
+        System.out.println("Produto: " + nome);
+        System.out.println("Preço: R$" + preco);
+        System.out.println("Quantidade em estoque: " + quantidade);
+    }
+  
+    public void exibirInformacoesDetalhadas(int quantidadeComprada) {
+        System.out.println("Produto: " + nome);
+        System.out.println("Preço: R$" + preco);
+        System.out.println("Quantidade em estoque: " + quantidade);
+        System.out.println("Quantidade comprada:" + quantidadeComprada + " por este cliente(s)");
+    }
+}
